df29e6bd0b9c26057fe2fece649fd1aa5b17cd9e
edoardope <pencoedoardo@gmail.com>
2023-04-14 16:23:53 +0200

    progetto finito

--- main.js
+++ main.js
@@ -31,10 +31,22 @@ const team = [
     },
 ];
 
-for ( i=0; i < team.length; i++ ) {
+let cards = ''; 
 
-    let teamMember = team[i]
+for (i = 0; i < team.length; i++) {
+  let teamMember = team[i];
 
-    console.log(`nome: ${ teamMember.name } ruolo: ${ teamMember.role } imgurl: ${ teamMember.image }`)
+  cards += `
+    <div class="card m-3" style="width: 18rem;">
+      <img src="asset/img/${ teamMember.image }" class="card-img-top" alt="img of ${ teamMember.name}">
+      <div class="card-body">
+        <h5 class="card-title">${ teamMember.name }</h5>
+        <p class="card-text">Role: ${ teamMember.role }</p>
+      </div>
+    </div>
+  `; 
+  
 }
+
+document.querySelector(".row").innerHTML = cards;
   
